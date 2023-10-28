261bcff78b70c58688ae9426a9d7a6e7abfe2f43
Antonio-Gonzalez-Gomez <antoniogg696@gmail.com>
2023-10-28 18:28:12 +0200

    Added rest of demo functionality

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 var zetta = require('zetta');
 var LED = require('zetta-led-mock-driver');
+var Photocell = require('zetta-photocell-mock-driver');
+var duskToDawnLight = require('./dusk_to_dawn');
 zetta()
   .name('Antonio-Gonzalez')
   .use(LED)
+  .use(Photocell)
+  .use(duskToDawnLight)
   .link('https://zetta-demo.onrender.com')
   .listen(1337, function(){
      console.log('Zetta is running at http://127.0.0.1:1337');
 });
-//https://zetta-browser.fly.dev
