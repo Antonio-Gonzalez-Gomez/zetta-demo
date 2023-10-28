c13decef88dcfa9aaba4b524ffea193d9b8a4891
Antonio González Gómez <61906376+Antonio-Gonzalez-Gomez@users.noreply.github.com>
2023-10-28 18:18:03 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,8 +3,7 @@ var LED = require('zetta-led-mock-driver');
 zetta()
   .name('Antonio-Gonzalez')
   .use(LED)
-  .link('http://hello-zetta.herokuapp.com/')
+  .link('https://zetta-demo.onrender.com')
   .listen(1337, function(){
      console.log('Zetta is running at http://127.0.0.1:1337');
 });
-//https://zetta-browser.fly.dev
